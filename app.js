b2cb4f3fe02fa4b169ca550ecc6aabe32cd310ab
Richi Thampan <richithampan@gmail.com>
2019-10-05 15:57:57 -0400

    made bookshelf dynamic based on current month and fixed flex issues

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const leftArrow = document.querySelector(".scroll-left");
 const bookShelf = document.querySelector(".book-shelf");
 const books = document.querySelector(".books");
 
-window.onload = () => updateScrollArrowVisibility(books);
+window.onload = () => {
+    addBooksToShelf();
+    updateScrollArrowVisibility(books)
+};
 
 
 rightArrow.addEventListener("click", ()=>{
@@ -21,6 +24,28 @@ books.addEventListener("scroll", () => {
 })
 
 
+
+function addBooksToShelf(){
+    const today = new Date();
+    const month = today.getMonth();
+    
+    for(let i = 1; i <= month + 1; i++){
+        const bookDate = new Date();
+        bookDate.setMonth(i - 1);
+        const monthName= Intl.DateTimeFormat('en-US', {month:"long"}).format(bookDate);
+        const fig = document.createElement("figure");
+        const img = document.createElement("img");
+        const imgSrc= "assets/book"+ i + ".jpg";
+        img.setAttribute("src", imgSrc )
+        img.setAttribute("alt", "book " + i );
+        const figCap = document.createElement("figcaption");
+        const figText= document.createTextNode(monthName + " Book of the Month");
+        figCap.append(figText);
+        fig.append(img, figCap);
+        books.insertBefore(fig, rightArrow)
+    }
+
+}
 
 function getScrollDirection(container) {
     const clientWidth = container.clientWidth;
